d7f90862a44cdfbff2d81209e808fe07436e05fd
HassanAbbas7 <ph015016test1@gmail.com>
2026-02-06 01:15:07 +0500

    fixes

--- constants/constants.jsx
+++ constants/constants.jsx
@@ -8,7 +8,7 @@ export const MAX_TIMETABLES = 5;
 export const API_URL = "https://hassanabbaspro.pythonanywhere.com/api";
 
 export const WHATSAPP1 = "+92 332 1877545";
-export const WHATSAPP2 = "+92 300 0000000";
+export const WHATSAPP2 = "+92 329 4596741";
 
 export const LS_COURSES_KEY = "tt_selected_courses";
 export const LS_TEACHERS_KEY = "tt_selected_teachers";
